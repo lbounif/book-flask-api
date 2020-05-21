78f2ac9cffe7016546fb52da0765ee5f648067e3
Lynda Bounif <l_bounif@esi.dz>
2020-05-21 19:27:59 +0100

    Add user's model

--- database/models.py
+++ database/models.py
@@ -5,3 +5,16 @@ class Book(db.Document):
     author = db.StringField(required=True)
     language = db.StringField(required=True)
     year = db.IntField(required=True)
+    added_by = db.ReferenceField('User')
+
+class User(db.Document):
+    email = db.EmailField(required=True, unique= True)
+    password = db.StringField(required= True, min_length=6)
+    books = db.ListField(db.ReferenceField('Book', reverse_delete_rule=db.PULL))
+
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
+    def check_password(self, password):
+         return check_password_hash(self.password, password)
+
+User.register_delete_rule(Book, 'added_by', db.CASCADE)

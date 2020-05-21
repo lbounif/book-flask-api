50d1ced5036e063f845232aeabff85430abf2701
Lynda Bounif <l_bounif@esi.dz>
2020-05-21 19:34:04 +0100

    Add routes and .env file

--- resources/routes.py
+++ resources/routes.py
@@ -1,5 +1,8 @@
 from .book import BooksApi, BookApi
+from .auth import SignupApi, LoginApi
 
 def initialize_routes(api):
  api.add_resource(BooksApi, '/api/books')
  api.add_resource(BookApi, '/api/books/<id>')
+ api.add_resource(SignupApi, '/api/auth/signup')
+ api.add_resource(LoginApi, '/api/auth/login')
